b18cfca72f80151fd53d4bd9904996a5072db572
Taha AKTAŞ <ta.aktas@hotmail.com>
2024-11-14 04:58:03 +0300

    Add Entities layer

---
+++ PlayReviewApp.PlayReviewDB.Entities/Concrete/News.cs
@@ -0,0 +1,33 @@
+﻿using PlayReviewApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayReviewApp.PlayReviewDB.Entities.Concrete
+{
+      public class News : IEntity
+      {
+            public int NewsId { get; set; }
+            public int CategoryId { get; set; }
+
+            public string Title { get; set; }
+
+            public string Content { get; set; }
+
+            public DateTime PublishedAt { get; set; }
+
+            public string ImageUrl { get; set; }
+
+            public string VideoUrl { get; set; }
+
+            public string Source { get; set; }
+
+            public string Author { get; set; }
+
+            public string Tags { get; set; }
+
+            public string Slug { get; set; }
+      }
+}
