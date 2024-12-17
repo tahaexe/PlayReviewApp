55633fc59dae2695a852ab6cc0c02ced1a41fc03
Taha AKTAŞ <ta.aktas@hotmail.com>
2024-12-18 01:24:07 +0300

    Update NewsDtoForGet.cs

--- PlayReviewApp.PlayReviewDb.Entities/DTO/News/NewsDtoForGet.cs
+++ PlayReviewApp.PlayReviewDb.Entities/DTO/News/NewsDtoForGet.cs
@@ -13,5 +13,6 @@ namespace PlayReviewApp.PlayReviewDb.Entities.DTO.News
         public string Content { get; set; }
         public string Description { get; set; }
         public string? ImageUrl { get; set; }
+        public string? Author { get; set; }
     }
 }
