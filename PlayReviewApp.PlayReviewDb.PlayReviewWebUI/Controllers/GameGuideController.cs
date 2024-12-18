d65633462748d606fef2f80ca3b198e52053cf8a
Taha AKTAŞ <ta.aktas@hotmail.com>
2024-12-18 04:18:03 +0300

    Add new Menus

---
+++ PlayReviewApp.PlayReviewDb.PlayReviewWebUI/Controllers/GameGuideController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayReviewApp.PlayReviewDb.PlayReviewWebUI.Controllers
+{
+    public class GameGuideController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PlayReviewApp.PlayReviewDb.PlayReviewWebUI/Controllers/HardwareController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PlayReviewApp.PlayReviewDb.PlayReviewWebUI.Controllers
+{
+    public class HardwareController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
