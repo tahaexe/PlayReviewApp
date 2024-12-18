8155ee154c70cf50f015b86db32a6cf1d86052d1
Taha AKTAŞ <ta.aktas@hotmail.com>
2024-12-18 04:17:27 +0300

    Update ApplicationExtension.cs

--- PlayReviewApp.PlayReviewDb.PlayReviewWebUI/Infrastructure/Extensions/ApplicationExtension.cs
+++ PlayReviewApp.PlayReviewDb.PlayReviewWebUI/Infrastructure/Extensions/ApplicationExtension.cs
@@ -49,37 +49,71 @@ namespace PlayReviewApp.PlayReviewDb.PlayReviewWebUI.Infrastructure.Extensions
             const string adminPassword = "Admin3541";
 
             UserManager<IdentityUser> userManager = app
-            .ApplicationServices
-            .CreateScope()
-            .ServiceProvider
-            .GetRequiredService<UserManager<IdentityUser>>();
+                .ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<UserManager<IdentityUser>>();
 
             RoleManager<IdentityRole> roleManager = app
-            .ApplicationServices
-            .CreateScope()
-            .ServiceProvider
-            .GetRequiredService<RoleManager<IdentityRole>>();
+                .ApplicationServices
+                .CreateScope()
+                .ServiceProvider
+                .GetRequiredService<RoleManager<IdentityRole>>();
 
+            string[] roles = { "ADMIN", "EDITOR", "USER" };
+
+            // Her rolü kontrol et ve yoksa oluştur
+            foreach (var role in roles)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+                if (!roleExist)
+                {
+                    var roleResult = await roleManager.CreateAsync(new IdentityRole(role));
+                    if (!roleResult.Succeeded)
+                        throw new Exception($"{role} role could not be created.");
+                }
+            }
+
+            // Admin kullanıcısını kontrol et
             IdentityUser user = await userManager.FindByNameAsync(adminUser);
             if (user is null)
             {
                 user = new IdentityUser()
                 {
                     Email = "ta.aktas@hotmail.com",
-                    PhoneNumber = "55534567",
+                    PhoneNumber = "000000000",
                     UserName = adminUser
                 };
 
+                // Kullanıcıyı oluştur
                 var result = await userManager.CreateAsync(user, adminPassword);
 
-                if (!result.Succeeded) throw new Exception("Admin user could not created.");
+                if (!result.Succeeded)
+                    throw new Exception("Admin user could not be created.");
 
-                var roleResult = await userManager.AddToRolesAsync(user,
-                      roleManager.Roles.Select(r => r.Name).ToList()
-                );
-
-                if (!roleResult.Succeeded) throw new Exception("System have problems with role defination for admin");
+                // Kullanıcıyı admin rolüne ekle
+                var roleResult = await userManager.AddToRolesAsync(user, roles);
+                if (!roleResult.Succeeded)
+                    throw new Exception("System have problems with role definition for admin");
             }
+        }
+
+
+        public static void ConfigureRouting(this WebApplication applicationBuilder)
+        {
+            //Areas
+            applicationBuilder.MapAreaControllerRoute(name: "Admin", areaName: "Admin", pattern: "Admin/{controller=Dasboard}/{action=index}/{id?}");
+
+            //Routes
+            applicationBuilder.MapControllerRoute(name: "News", pattern: "oyunhaberleri/{action=Index}/{id?}", defaults: new { controller = "News" });
+            applicationBuilder.MapControllerRoute(name: "Review", pattern: "incelemeler/{action=Index}/{id?}", defaults: new { controller = "Review" });
+            applicationBuilder.MapControllerRoute(name: "Document", pattern: "dosya/{action=Index}/{id?}", defaults: new { controller = "Document" });
+            applicationBuilder.MapControllerRoute(name: "TV", pattern: "tv/{action=Index}/{id?}", defaults: new { controller = "TV" });
+            applicationBuilder.MapControllerRoute(name: "Interview", pattern: "roportaj/{action=Index}/{id?}", defaults: new { controller = "Interview" });
+            applicationBuilder.MapControllerRoute(name: "Hardware", pattern: "donanim/{action=Index}/{id?}", defaults: new { controller = "Hardware" });
+            applicationBuilder.MapControllerRoute(name: "GameGuide", pattern: "rehber/{action=Index}/{id?}", defaults: new { controller = "GameGuide" });
+            applicationBuilder.MapControllerRoute(name: "Magazine", pattern: "dergi/{action=Index}/{id?}", defaults: new { controller = "Magazine" });
+            applicationBuilder.MapControllerRoute(name: "Home", pattern: "{controller=Home}/{action=Index}/{id?}");
         }
     }
 }
